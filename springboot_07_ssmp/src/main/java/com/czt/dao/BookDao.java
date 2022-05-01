04bf8f18a3818086d5c33d1d61696b3a9bd66c04
Tao135 <1295072844@qq.com>
2022-05-01 23:36:28 +0800

    @Tao135 Springboot整合SSM之配置以及CRUD篇 操作步骤：可在配置文件yml中设置表中id自增，以及配置表名映射，dao层继承BaseMapper

---
+++ springboot_07_ssmp/src/main/java/com/czt/dao/BookDao.java
@@ -0,0 +1,11 @@
+package com.czt.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.czt.pojo.Book;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface BookDao extends BaseMapper<Book> {
+}
