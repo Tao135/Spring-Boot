ee67c33b2c78a4e886aaf78ef240dfe2d936ff3c
Tao135 <1295072844@qq.com>
2022-05-16 20:12:53 +0800

    @Tao135 SpringBoot与Maven多环境 注意事项：1.在pom.xml文件中<properties>下的<profile.active>就如设置了这个变量，里面用的则是值，<profile.active>dev</profile.active> 2.在主配置文件中，使用@profile.active@进行使用这个环境。 3.基于SpringBoot读取Maven配置属性的前提下，如果在Idea下测试工程时pom.xml每次更新需要手动compile方可生效

---
+++ springboot_08_profiles/src/main/java/com/czt/Springboot08ProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.czt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot08ProfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot08ProfilesApplication.class, args);
+    }
+
+}
