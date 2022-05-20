51ccd0f7d8caeaa7ae460f6c724a6699049db1c0
Tao135 <1295072844@qq.com>
2022-05-20 21:58:47 +0800

    @Tao135 SpringBoot注解形式日志 操作步骤：在pom.xml文件中注入lombok，然后在类上使用@Slf4j，直接使用log._(debug)（日志形式）即可。

--- springboot_09_log/src/main/java/com/czt/controller/BookController.java
+++ springboot_09_log/src/main/java/com/czt/controller/BookController.java
@@ -1,18 +1,19 @@
 package com.czt.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-    //创建记录日志的对象
-    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+//    //创建记录日志的对象
+//    private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
 
     @GetMapping
